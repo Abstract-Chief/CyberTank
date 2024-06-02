e610a5428a5e59b610a2fe48e0514021020a8454
Hryhorii Sukhikh <hryhorii.sukhikh@student.tuke.sk>
2024-06-02 13:45:26 +0200

    minimal work tank version

--- Debugger/.ccls-cache/@home@abstarct@Games@Debugger/debug.h
+++
@@ -1,7 +0,0 @@
-/*File "debug.h" create by abstarct, (нд, 03-гру-2023 15:59:18 +0200)*/
-#ifndef ABSTARCT_DEBUG_H_SENTURY
-#define ABSTARCT_DEBUG_H_SENTURY
-
-int InitDebugerOutput(int n,const char *name);
-int erprintf(int you_fd,const char *format,...);
-#endif
--- main.c
+++ main.c
@@ -1,41 +1,10 @@
 /*File "main.c" create by abstarct, (ср, 06-гру-2023 16:14:09 +0200)*/
-#include <curses.h>
-#include <stdbool.h>
-#include "game_module.h"
-void init(){
-   initscr();
-   curs_set(0);
-   nocbreak();
-   nodelay(stdscr,true);
-   keypad(stdscr,1);
-}
-int close(){
-   endwin();
-   return 0;
-}
-typedef struct{
-   float x,y;
-   int hp;
-   float reload;
-} Tank;
-typedef struct{
-   float x,y;
-   float dx,dy;
-   Tank* tank;
-} Bullet;
-
-void graphick_tank(Tank* tank){
-
-}
-int game(GameEngine *engine,int mils){
-   static int n=0;
-   mvprintw(0,0,"%d %d\n",mils,++n);
-   refresh();
-   return 0;
-}
+#include "game.h"
 int main(){
    init();
+   init_graphick_obj();
    GameEngine* engine = create_game_engine();
+   init_storage(engine->storage);
    add_handler(engine,game,"game");
    game_module(engine,30);
    return close();
